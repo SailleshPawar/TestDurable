21700bba3c3688277491cd0dce88ea02441b1743
Saillesh Pawar <Sailleshp@quinnox.com>
2019-04-25 20:42:37 +0530

    Implemented authorization filter for function

---
+++ TestDurable/AuthorizeAttribute.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestDurable
+{
+
+
+    public class ErrorHandlerAttribute : FunctionExceptionFilterAttribute
+    {
+        public override Task OnExceptionAsync(
+            FunctionExceptionContext exceptionContext, CancellationToken cancellationToken)
+        {
+            // custom error handling logic could be written here
+            // (e.g. write a queue message, send a notification, etc.)
+
+            exceptionContext.Logger.LogError(
+                $"ErrorHandler called. Function '{exceptionContext.FunctionName}" +
+                ":{exceptionContext.FunctionInstanceId} failed.");
+
+            return Task.CompletedTask;
+        }
+    }
+
+        public class FunctionAuthorizeAttribute : FunctionInvocationFilterAttribute
+    {
+        public FunctionAuthorizeAttribute()
+        {
+        }
+
+        public override Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken)
+        {
+            var workItem = executingContext.Arguments.First().Value as HttpRequestMessage;
+            ValidationPackage validationPackage = new ValidationPackage();
+            AuthenticationHeaderValue jwtInput = workItem.Headers.Authorization;
+
+            if (jwtInput != null)
+            {
+                String jwt = "";
+                if (jwtInput.ToString().StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+                {
+                    jwt = jwtInput.ToString().Substring("Bearer ".Length).Trim();
+                }
+
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+                if (!string.IsNullOrEmpty(jwt))
+                {
+                    try
+                    {
+                        validationPackage = ExtractClaims(jwt, handler);
+                    }
+                    catch (Exception e)
+                    {
+                        // log.Error("Exception caught while validating token", e);
+
+                    }
+                }
+                else
+                {
+                    //log.Error("Auth Token begins with [" + jwtInput.ToString().Substring(0, 12) + "...]");
+
+                }
+            }
+
+            if(!validationPackage.validToken)
+            {
+                workItem.Headers.Add("AuthorizationStatus",  Convert.ToInt32(HttpStatusCode.Unauthorized).ToString());
+            }
+            else
+            {
+                workItem.Headers.Add("AuthorizationStatus", Convert.ToInt32(HttpStatusCode.Accepted).ToString());
+           
+            }
+
+            return base.OnExecutingAsync(executingContext, cancellationToken);
+        }
+
+
+        public static ValidationPackage ExtractClaims(string jwt, JwtSecurityTokenHandler handler)
+        {
+            ValidationPackage validationPackage = new ValidationPackage();
+
+            validationPackage.token = jwt;
+
+            var token = handler.ReadJwtToken(jwt);
+
+            // !!!!!!!! Hardcoded Scope !!!!!!!!! \\
+            validationPackage.scope = "user_impersonation";
+
+            try
+            {
+                //Extract the payload of the JWT
+                var claims = token.Claims;
+
+                foreach (Claim c in claims)
+                {
+                    switch (c.Type)
+                    {
+                        case "sub":
+                        case "upn":
+                            // Make sure it's an email address ...
+                            if (c.Value.Contains('@'))
+                                validationPackage.principalName = c.Value;
+                            //log.Info(Logger.Header() + "upn=" + c.Value);
+                            break;
+
+                        case "Firstname":
+                            validationPackage.firstName = c.Value;
+                            //log.Info(Logger.Header() + "Firstname=" + c.Value);
+                            break;
+
+                        case "Lastname":
+                            validationPackage.lastName = c.Value;
+                            //log.Info(Logger.Header() + "Lastname=" + c.Value);
+                            break;
+
+                        case "client_id":
+                        case "aud":
+                            validationPackage.appID = c.Value;
+                            //log.Info(Logger.Header() + "aud=" + c.Value);
+                            break;
+
+                        case "iat":
+                            validationPackage.issuedAt = Convert.ToInt64(c.Value);
+                            //log.Info(Logger.Header() + "iat=" + c.Value);
+                            break;
+
+                        case "exp":
+                            validationPackage.expiresAt = Convert.ToInt64(c.Value);
+                            //log.Info(Logger.Header() + "exp=" + c.Value);
+                            break;
+
+                        case "scp":
+                            validationPackage.scope = c.Value;
+                            //log.Info(Logger.Header() + "scp=" + c.Value);
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                validationPackage.validToken = false;
+            }
+
+            var currentTimestamp = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
+
+            if ((validationPackage.expiresAt - currentTimestamp) > 0)
+                validationPackage.validToken = true;
+
+            return validationPackage;
+        }
+
+    }
+}
--- TestDurable/GetCustomerById.cs
+++ TestDurable/GetCustomerById.cs
@@ -15,6 +15,7 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using TestDurable;
 using System.Security.Claims;
+using System.Net;
 
 namespace TestDurable
 {
@@ -22,14 +23,12 @@ namespace TestDurable
     public class ValidationPackage
     {
         public bool validToken { get; set; }
-        public String principalName { get; set; }
-        public String scope { get; set; }
-        public String firstName { get; set; }
-        public String lastName { get; set; }
-        public String appID { get; set; }
+        public string principalName { get; set; }
+        public string scope { get; set; }
+        public string appID { get; set; }
         public long issuedAt { get; set; }
         public long expiresAt { get; set; }
-        public String token { get; set; }
+        public string token { get; set; }
 
         public ValidationPackage()
         {
@@ -39,138 +38,44 @@ namespace TestDurable
 
   
 
+   
     public static class GetCustomerById
     {
 
-        public static ValidationPackage ExtractClaims(string jwt, JwtSecurityTokenHandler handler)
-        {
-            ValidationPackage validationPackage = new ValidationPackage();
-
-            validationPackage.token = jwt;
-
-            var token = handler.ReadJwtToken(jwt);
-
-            // !!!!!!!! Hardcoded Scope !!!!!!!!! \\
-            validationPackage.scope = "user_impersonation";
-
-            try
-            {
-                //Extract the payload of the JWT
-                var claims = token.Claims;
-
-                foreach (Claim c in claims)
-                {
-                    switch (c.Type)
-                    {
-                        case "sub":
-                        case "upn":
-                            // Make sure it's an email address ...
-                            if (c.Value.Contains('@'))
-                                validationPackage.principalName = c.Value;
-                            //log.Info(Logger.Header() + "upn=" + c.Value);
-                            break;
-
-                        case "Firstname":
-                            validationPackage.firstName = c.Value;
-                            //log.Info(Logger.Header() + "Firstname=" + c.Value);
-                            break;
-
-                        case "Lastname":
-                            validationPackage.lastName = c.Value;
-                            //log.Info(Logger.Header() + "Lastname=" + c.Value);
-                            break;
-
-                        case "client_id":
-                        case "aud":
-                            validationPackage.appID = c.Value;
-                            //log.Info(Logger.Header() + "aud=" + c.Value);
-                            break;
-
-                        case "iat":
-                            validationPackage.issuedAt = Convert.ToInt64(c.Value);
-                            //log.Info(Logger.Header() + "iat=" + c.Value);
-                            break;
-
-                        case "exp":
-                            validationPackage.expiresAt = Convert.ToInt64(c.Value);
-                            //log.Info(Logger.Header() + "exp=" + c.Value);
-                            break;
-
-                        case "scp":
-                            validationPackage.scope = c.Value;
-                            //log.Info(Logger.Header() + "scp=" + c.Value);
-                            break;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                validationPackage.validToken = false;
-            }
-
-            var currentTimestamp = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
-
-            if ((validationPackage.expiresAt - currentTimestamp) > 0)
-                validationPackage.validToken = true;
-
-            return validationPackage;
-        }
-
+       
         [FunctionName("GetCustomerById")]
+        [FunctionAuthorize]
+        [ErrorHandler]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            ValidationPackage validationPackage = null;
-            AuthenticationHeaderValue jwtInput = req.Headers.Authorization;
 
-            if (jwtInput != null)
+            string authorizationStatus = req.Headers.GetValues("AuthorizationStatus").FirstOrDefault();         
+            if (Convert.ToInt32(authorizationStatus).Equals((int)HttpStatusCode.Accepted))
             {
-                String jwt = "";
-                if (jwtInput.ToString().StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
-                {
-                    jwt = jwtInput.ToString().Substring("Bearer ".Length).Trim();
-                }
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                var customers = Builder<Customer>
+                .CreateListOfSize(10)
+                .All().Build();
+                //int Id =Convert.ToInt32( req.Query["Id"]);
+                int Id = 1;
 
-                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                var customer = customers.Where(x => x.Id == Id);
 
-                if (!string.IsNullOrEmpty(jwt))
-                {
-                    try
-                    {
-                        validationPackage = ExtractClaims(jwt, handler);
-                    }
-                    catch (Exception e)
-                    {
-                       // log.Error("Exception caught while validating token", e);
-                       
-                    }
-                }
-                else
-                {
-                    //log.Error("Auth Token begins with [" + jwtInput.ToString().Substring(0, 12) + "...]");
-                   
-                }
+                //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                //dynamic data = JsonConvert.DeserializeObject(requestBody);
+                //name = name ?? data?.name;
+
+                return customer != null
+                    ? (ActionResult)new OkObjectResult(customer)
+                    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
             }
+            else
 
-            
-
-           var customers= Builder<Customer>
-           .CreateListOfSize(10)
-           .All().Build();
-            //int Id =Convert.ToInt32( req.Query["Id"]);
-            int Id = 1;
-
-            var customer = customers.Where(x => x.Id == Id);
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            return customer != null
-                ? (ActionResult)new OkObjectResult(customer)
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            {
+                return new UnauthorizedResult();
+            }
         }
 
 
